c2939af301a15829e4b4352d7cfea9b8a200d8be
jKm00 <joakimedvardsen2000@gmail.com>
2023-02-28 17:24:56 +0100

    country-challenge: small fixes

--- countries-challenge/countries-svelte/src/routes/+page.svelte
+++ countries-challenge/countries-svelte/src/routes/+page.svelte
@@ -17,7 +17,15 @@
 </script>
 
 <nav class="nav">
-	<input class="search" type="text" placeholder="Search for a country..." bind:value={input} />
+	<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+	<div class="search--wrapper">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+			><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+				d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
+			/></svg
+		>
+		<input class="search" type="text" placeholder="Search for a country..." bind:value={input} />
+	</div>
 	<div class="dropdown-wrapper">
 		<svg
 			data-active-dropdown={activeDropdown}
@@ -49,7 +57,7 @@
 
 <section class="grid">
 	{#each filteredCountries as country}
-		<a class="card" href={`/countries/${country.name.common.toLowerCase()}`}>
+		<a class="card" href={`/countries/${encodeURIComponent(country.name.common.toLowerCase())}`}>
 			<img
 				class="flag"
 				src={country.flags.png}
@@ -76,17 +84,37 @@
 		margin-block: 2rem;
 	}
 
-	.search {
-		border: none;
-		padding: 1rem 1.5rem;
+	.search--wrapper {
+		display: flex;
+		gap: 1rem;
+		padding: 1rem;
 		border-radius: var(--border);
 		box-shadow: var(--shadow-200);
 		background-color: var(--clr-el);
+		width: min(100%, 25rem);
+	}
+
+	.search--wrapper:focus-within {
+		outline: 2px solid var(--clr-fg);
+	}
+
+	.search--wrapper svg {
+		width: 1rem;
+	}
+
+	.search {
+		border: none;
+		background-color: transparent;
 		color: var(--clr-fg);
 	}
 
 	.search::placeholder {
 		color: var(--clr-fg);
+	}
+
+	.search:hover,
+	.search:focus {
+		outline: none;
 	}
 
 	.dropdown-wrapper {
