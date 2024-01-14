f22400e5060b9eed1ed04500d1a1656156d82da3
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-01-14 22:51:09 +0100

    init: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,49 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		screens: {
+			sm: '375px',
+			md: '768px',
+			lg: '1440px'
+		},
+		extend: {
+			colors: {
+				border: 'hsl(var(--border))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				success: {
+					DEFAULT: 'hsl(var(--success))',
+					foreground: 'hsl(var(--success-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				}
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			fontFamily: {
+				sans: [...fontFamily.sans]
+			}
+		}
 	},
 	plugins: []
 };
